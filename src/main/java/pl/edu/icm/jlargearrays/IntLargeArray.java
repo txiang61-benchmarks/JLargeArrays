39048099df051a95aeb1080492daeb10c3d1edf3
piotrw <piotrw@hoth.lab.icm.edu.pl>
2013-05-13 15:01:18 +0200

    License changed from LGPL to GPL with classpath exception.

--- src/main/java/pl/edu/icm/jlargearrays/IntLargeArray.java
+++ src/main/java/pl/edu/icm/jlargearrays/IntLargeArray.java
@@ -1,20 +1,44 @@
 /* ***** BEGIN LICENSE BLOCK *****
  * 
- * JLargeArrays is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
+ * JLargeArrays
+ * Copyright (C) 2013 University of Warsaw, ICM
  *
- * JLargeArrays is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This file is part of GNU Classpath.
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this Module; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Classpath; see the file COPYING.  If not, write to the 
+ * University of Warsaw, Interdisciplinary Centre for Mathematical and 
+ * Computational Modelling, Pawinskiego 5a, 02-106 Warsaw, Poland. 
+ * 
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ * 
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this library, you may extend
+ * this exception to your version of the library, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version. 
+ * 
  * ***** END LICENSE BLOCK ***** */
+
 
 package pl.edu.icm.jlargearrays;
 
